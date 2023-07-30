dbb827f066f05aaaaec810045813c960f0cfabb2
professor_mr <professormr29@gmail.com>
2023-07-30 13:17:41 +0330

    add template 5

---
+++ template-5/assets/js/index.js
@@ -0,0 +1,16 @@
+//full page
+
+new fullpage('#fullpage',{
+    autoScrolling:true,
+    navigation: true,
+    navigationTooltips: [
+        "صفحه نخست",
+        "خدمات ما",
+        "محصولات ما",
+        "تماس با ما",
+        "درباره ما"
+    ],
+    showActiveTooltip: true,
+});
+
+//full page
